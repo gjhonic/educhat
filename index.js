3851232b125fa3c8489a713faef65f340b199a72
gjhonic <gjhonic@gmail.com>
2021-02-27 20:02:13 +0800

    add layouts

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ app.use(express.static(path.join(__dirname, 'src')));
 
 let urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+
 app.set('view engine', 'ejs');
+app.set('layout', 'views/layouts');
 app.use('/public',express.static('public'));
+
 
 
 //Главная страница >>>
